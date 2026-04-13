8c19f33d7d3c653d2822391b8c8ca39a178f9def
xiaomei011206-beep <xiaomei011206@gmail.com>
2026-04-13 18:03:31 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Menampilkan teks
+    printf("Hello, World!\n");
+
+    // Variabel
+    int umur;
+
+    // Input dari user
+    printf("Masukkan umur kamu: ");
+    scanf("%d", &umur);
+
+    // Output hasil
+    printf("Umur kamu adalah %d tahun.\n", umur);
+
+    return 0;
+}
